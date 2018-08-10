35c93bcb995358c2fc68b1a73efac3fd8b7f2f6d
Pedro Henrique de Miranda Arthur <pedrohma95@gmail.com>
2018-08-10 15:58:29 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import timeit
+import csv
+with open('dataset.csv') as csv_file:
+    start = timeit.default_timer()
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\tThe GameID {row[0]} is on Season {row[12]}.')
+            line_count += 1
+    stop = timeit.default_timer()
+    print(f'Processed {line_count} lines in {stop - start } seconds.')
+
+# import sys
+#
+# for line in sys.stdin:
+#     line = line.strip()
+#     unpacked = line.split(",")
+#     stadium, capacity, expanded, location, surface, turf, team, opened, weather, roof, elevation = line.split(",")
+#     results = [turf, "1"]
+#     print("\t".join(results))
